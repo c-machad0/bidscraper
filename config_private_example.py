caf0fcf4129ce8c9f794d576e04cee0e27c83604
Christian Machado <realchris.machado@gmail.com>
2025-10-08 21:24:37 -0300

    Consertando algumas importações

--- config_private_example.py
+++ config_private_example.py
@@ -2,7 +2,6 @@ import os
 
 # Variáveis de ambiente para Railway
 my_token = os.getenv('MY_TOKEN')
-my_chat_id = os.getenv('MY_CHAT_ID') 
 chat_id_channel = os.getenv('CHAT_ID_CHANNEL')
 
 # Verificação se as variáveis foram carregadas
